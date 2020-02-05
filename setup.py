0bf7530e2cd17b522257d390221267e24e727a40
sterbini <guido.sterbini@cern.ch>
2020-02-05 22:24:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='lumimodel',
       version='0.01',
-      description='LHC ',
+      description='LHC luminosity model',
       url='https://github.com/sterbini/lumimodel',
       author='Guido STERBINI',
       author_email='guido.sterbini@cern.ch',
