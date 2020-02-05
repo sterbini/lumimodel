fb012718c08cefccd2a728e56b55445056d9f2c9
sterbini <guido.sterbini@cern.ch>
2020-02-05 22:22:18 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lumimodel',
+      version='0.01',
+      description='LHC ',
+      url='https://github.com/sterbini/lumimodel',
+      author='Guido STERBINI',
+      author_email='guido.sterbini@cern.ch',
+      license='MIT',
+      packages=['luminodel'],
+      zip_safe=False)
